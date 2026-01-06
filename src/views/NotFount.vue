8a2e62a9edf89549aca6e3379fef866f553e950f
yangdanping <1240645840@qq.com>
2026-01-06 21:12:37 +0800

    chore: 更新依赖项并优化NotFound

--- src/views/NotFount.vue
+++ src/views/NotFount.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="">
-    <div class="">NotFount</div>
+  <div class="not-found">
+    <div class="content">
+      {{ textStr }}
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const text = ref('404 Not Found');
+const textStr = ref('');
+const counter = ref(1);
+let timer = ref();
 
-<style lang="scss" scoped></style>
+onMounted(() => {
+  timer.value = setInterval(() => {
+    let str = text.value.slice(0, counter.value);
+    counter.value++;
+    if (counter.value <= text.value.length + 1) {
+      str = str + '|';
+    } else {
+      clearInterval(timer.value);
+    }
+    textStr.value = str;
+  }, 100);
+});
+
+onUnmounted(() => {
+  if (timer.value) clearInterval(timer.value);
+});
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: clamp(40px, 10vw, 70px);
+  // font-family: 'MapleMono', sans-serif;
+  color: #5f5f5f;
+}
+</style>
