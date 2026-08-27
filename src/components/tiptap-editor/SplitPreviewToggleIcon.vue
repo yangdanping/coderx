7a0c2550b2d6fd1a88e983a40ddf77aa7f6559bc
yangdanping <1240645840@qq.com>
2026-08-27 12:42:27 +0800

    fix(editor): align split preview icon actions

--- src/components/tiptap-editor/SplitPreviewToggleIcon.vue
+++ src/components/tiptap-editor/SplitPreviewToggleIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 四角括号：外扩表示打开分栏，内收表示关闭分栏 -->
-  <Minimize v-if="!expanded" class="split-preview-toggle-icon" :size="16" aria-hidden="true" />
-  <Maximize v-else class="split-preview-toggle-icon" :size="16" aria-hidden="true" />
+  <Maximize v-if="!expanded" class="split-preview-toggle-icon" :size="16" aria-hidden="true" />
+  <Minimize v-else class="split-preview-toggle-icon" :size="16" aria-hidden="true" />
 </template>
 
 <script lang="ts" setup>
